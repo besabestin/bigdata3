4b014dbf4c7ab5ab6279c7ef8974ad9ade673df1
Think ML <thinkml@Bisrats-MacBook-Pro.local>
2024-10-16 08:33:15 +0200

    some more cleanup

--- shopper/frontend/src/router/index.js
+++ shopper/frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import NeedToBuy from '@/views/NeedToBuy.vue'
 
 const router = createRouter({
